acf74e0fb23cdf85dc2b9890e3be27ee59419045
John Rasine Irem <rajonirem8@gmail.com>
2023-04-19 06:27:57 +0100

    initialized intro components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import IntroNav from './components/IntroNav/IntroNav'
+function App() {
+
+  return (
+    <div className="App">
+      <IntroNav />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/IntroNav/IntroNav.jsx
@@ -0,0 +1,12 @@
+import { IntroWrapper } from "./IntroNavStyles"
+
+const IntroNav = () => {
+  return (
+    <IntroWrapper>
+      <h1>
+        Hey, my name is John Irem.
+      </h1>
+    </IntroWrapper>
+  )
+}
+export default IntroNav
