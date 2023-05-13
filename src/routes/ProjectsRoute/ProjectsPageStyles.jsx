857d6d024fb2c4326a74f704058b6ecb10f92c0a
John Rasine Irem <rajonirem8@gmail.com>
2023-05-13 18:50:01 +0100

    feat: styled buttons

--- src/routes/ProjectsRoute/ProjectsPageStyles.jsx
+++ src/routes/ProjectsRoute/ProjectsPageStyles.jsx
@@ -1,7 +1,11 @@
 import styled from "styled-components";
 
 export const ProjectsPageIntroWrap = styled.div`
-  @media (max-width: 1200px) {
-    /* display: none; */
+  & button{
+    background-color: #f4eee0;
+  color: #393646;
+  padding: 3px;
+  border: 1px solid #393646;
+  margin: 0 2px;
   }
 `
