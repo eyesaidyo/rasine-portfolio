38e5ab69733d690aa3d5816effc45704503a6f35
John Rasine Irem <rajonirem8@gmail.com>
2024-03-01 15:28:09 +0100

    Update Projects.jsx
    remove basic proj

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,24 +7,25 @@ const Projects = () => {
     <MainProjectsWrapper>
       <h1>Projects</h1>
       <ProjectItem title={'Chatter'} summary='a social media site for text-based content (...in progress)' repo='https://github.com/eyesaidyo/rasine-chatter' live='https://rasine-chatter.netlify.app/' />
-      <ProjectItem title={'to-do vue'}
+{/*       <ProjectItem title={'to-do vue'}
         summary='basic to-do list app in vue'
         repo='https://github.com/eyesaidyo/todo-vue'
         live='https://rasine-todo-vue.netlify.app/'
 
-      />
-      <ProjectItem title={'github checker react'} summary='a project fetching all my github repos and displaying them.' repo={'https://github.com/eyesaidyo/my-repos-2'} live='https://eyesaidyo.netlify.app/' />
+      /> */}
+{/*       <ProjectItem title={'github checker react'} summary='a project fetching all my github repos and displaying them.' repo={'https://github.com/eyesaidyo/my-repos-2'} live='https://eyesaidyo.netlify.app/' />
+       */}
       <ProjectItem title={'meta-bnb'} summary='landing page for a metaverse real-estate directory'
         repo='https://github.com/eyesaidyo/metabnb' live='https://rajon-stage3.netlify.app/'
       />
 
-      <ProjectItem title={'github checker vue'} summary='a project fetching all my github repos done in vue js' repo='https://github.com/eyesaidyo/alt-school-exam-vue' live='https://rasine-vue.netlify.app/' />
+{/*       <ProjectItem title={'github checker vue'} summary='a project fetching all my github repos done in vue js' repo='https://github.com/eyesaidyo/alt-school-exam-vue' live='https://rasine-vue.netlify.app/' />
       <ProjectItem title={'to-do vue'}
         summary='basic to-do list app in vue'
         repo='https://github.com/eyesaidyo/todo-vue'
         live='https://rasine-todo-vue.netlify.app/'
 
-      />
+      /> */}
       <ProjectsPageIntroWrap>
         <Link to='/'><button>Home</button></Link>
         <Link to='/articles'><button>Articles</button></Link>
