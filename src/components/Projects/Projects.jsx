f235bf8ca58f04d0e55a83e4b0c2b8478ce31211
John Rasine Irem <rajonirem8@gmail.com>
2024-09-17 15:47:27 +0100

    beddings

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -7,22 +7,21 @@ const Projects = () => {
     <MainProjectsWrapper>
       <h1>Projects</h1>
       <ProjectItem title={'Chatter'} summary='a social media site for text-based content (...in progress)' repo='https://github.com/eyesaidyo/rasine-chatter' live='https://rasine-chatter.netlify.app/' />
-      <ProjectItem title={'to-do vue'}
+      {/* <ProjectItem title={'to-do vue'}
         summary='basic to-do list app in vue'
         repo='https://github.com/eyesaidyo/todo-vue'
         live='https://rasine-todo-vue.netlify.app/'
 
-      />
-      <ProjectItem title={'github checker react'} summary='a project fetching all my github repos and displaying them.' repo={'https://github.com/eyesaidyo/my-repos-2'} live='https://eyesaidyo.netlify.app/' />
+      /> */}
+      {/* <ProjectItem title={'github checker react'} summary='a project fetching all my github repos and displaying them.' repo={'https://github.com/eyesaidyo/my-repos-2'} live='https://eyesaidyo.netlify.app/' /> */}
       <ProjectItem title={'meta-bnb'} summary='landing page for a metaverse real-estate directory'
         repo='https://github.com/eyesaidyo/metabnb' live='https://rajon-stage3.netlify.app/'
       />
 
-      <ProjectItem title={'github checker vue'} summary='a project fetching all my github repos done in vue js' repo='https://github.com/eyesaidyo/alt-school-exam-vue' live='https://rasine-vue.netlify.app/' />
-      <ProjectItem title={'to-do vue'}
-        summary='basic to-do list app in vue'
-        repo='https://github.com/eyesaidyo/todo-vue'
-        live='https://rasine-todo-vue.netlify.app/'
+      <ProjectItem title={'Beddings Landing Page'}
+        summary='landing page for online beddings shop'
+        repo='https://github.com/eyesaidyo/stage2'
+        live='https://hng2-rasine.netlify.app/'
 
       />
       <ProjectsPageIntroWrap>
